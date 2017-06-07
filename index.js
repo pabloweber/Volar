77629c08d120ec2ca68372d3495529b08f25f983
Pablo Weber <pabloliweber@gmail.com>
2017-06-07 19:40:36 +0800

    "EVENTS" section added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $(window).ready(function(){
 
+	(function changeSocialSize(){	
 	 	if ($(window).height() < 800) { 
 	 		 $('.fa').addClass('fa-2x');
 	 	} else {
 	 		$('.fa').addClass('fa-3x');
 	 	}
+	})();
 
 })
